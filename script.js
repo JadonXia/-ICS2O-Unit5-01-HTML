873dadad7c49109e6b42e1b08eab94e9d023d34b
JadonXia <jadon.xia@ocsbstudent.ca>
2021-03-16 17:08:58 +0000

    added new file

---
+++ script.js
@@ -0,0 +1,10 @@
+// This script will change paragraph colors.
+document.getElementById('paragraph1').style.color = 'purple'
+// This script will test out style elements
+document.getElementById('paragraph2').style.color = 'blue'
+document.getElementById('paragraph2').style.border = 'solid black 2px'
+document.getElementById('paragraph2').style.position = 'fixed'
+document.getElementById('paragraph2').style.bottom = '0%'
+document.getElementById('paragraph2').style.left = '0%'
+document.getElementById('paragraph2').style.width = '15%'
+document.getElementById('paragraph2').style.padding = '2px'
